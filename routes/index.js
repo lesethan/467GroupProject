b07dd1d4adb0829327a2a09d47da89a31840f4c1
2luis <89538536+2luis@users.noreply.github.com>
2022-11-19 17:58:54 -0600

    added catalog (not finished yet), put styles from .html into style.css, changed index from .html to ejs

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,27 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
 // Homepage Route
-router.get("/", (req, res) => {
-  res.render("index");
+router.get('/', (req, res) => {
+  res.render('index');
 });
 
-const parts = require("../config/db");
-// Backend Data Testing Route
-router.get("/test", (req, res) => {
+const parts = require('../config/db');
+router.get('/catalog', (req, res) => {
   try {
     parts.getAllItems((list) => {
-      res.render("test", { all: list });
+      res.render('catalog', { all: list });
+    });
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+});
+// Backend Data Testing Route
+router.get('/test', (req, res) => {
+  try {
+    parts.getAllItems((list) => {
+      res.render('test', { all: list });
     });
   } catch (error) {
     console.log(error);
